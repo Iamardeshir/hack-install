6db473a47a7b4fafd747b89c710b639f7202152d
Iamardeshir <85497009+Iamardeshir@users.noreply.github.com>
2021-06-07 17:32:39 +0430

    Add files via upload

---
+++ hackinstall.sh
@@ -0,0 +1,381 @@
+clear
+
+apt update
+
+apt install figlet -y
+
+clear
+
+figlet hackinstall
+
+echo Press enter to contnue. . .
+
+read a
+
+clear
+
+figlet hackinstall
+
+echo y | apt upgrade -y
+
+clear
+
+figlet hackinstall
+
+apt install git -y
+
+clear
+
+figlet hackinstall
+
+apt install wget -y
+
+clear
+
+figlet hackinstall
+
+apt install proot -y
+
+clear
+
+figlet hackinstall
+
+git clone https://github.com/MFDGaming/ubuntu-in-termux
+
+clear
+
+figlet hackinstall
+
+cd ubuntu-in-termux
+
+clear
+
+figlet hackinstall
+
+chmod +x ubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo y | ./ubuntu.sh
+
+clear
+
+#update
+
+figlet hackinstall
+
+echo mkdir programs | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo "mkdir scripts && cd scripts && mkdir python && mkdir ruby && mkdir c && mkdir c++ && mkdir c# && cd .. " ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo mkdir downloads | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo mkdir documents | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo "echo Please enter a password to continue. . ." | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo passwd | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt update | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt upgrade -y  | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+#crack
+
+clear
+
+figlet hackinstall
+
+echo apt install hashcat -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install hydra -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install aircrack-ng -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install john -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+#wireless
+
+clear
+
+figlet hackinstall
+
+echo apt install pciutils -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install kmod -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install wireless-tools | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install net-tools | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install wifite -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install aircrack-ng -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install wireshark -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+#virus
+
+clear
+
+figlet hackinstall
+
+echo apt install nano -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install steghide -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+#connection
+
+clear
+
+figlet hackinstall
+
+echo apt install ftp -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install ssh -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install telnet -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install nmap -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install netcat -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+#work
+
+clear
+
+figlet hackinstall
+
+echo apt install cmatrix -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install moon-buggy -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install figlet -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install macchanger -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install sudo -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install git -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install wget -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install curl -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+#programing
+
+clear
+
+figlet hackinstall
+
+echo apt install python -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install python2 -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install python3 -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install ruby -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install gcc -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+echo apt install g++ -y | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+#gits
+
+clear
+
+figlet hackinstall
+
+#cant install Tool-X hxwhatsapp instahack gemailhack hunner
+
+clear
+
+figlet hackinstall
+
+#we need to install with git
+
+clear
+
+figlet hackinstall
+
+echo "cd programs && git clone https://github.com/rajkumardusad/Tool-X&& git clone https://github.com/Bl4ckDr460n/HxWhatsApp && git clone https://github.com/fuck3erboy/instahack && git clone https://github.com/Ha3MrX/Gemail-Hack && git clone https://github.com/b3-v3r/Hunner && git clone https://github.com/evait-security/weeman && wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh " | ./startubuntu.sh
+
+clear
+
+figlet hackinstall
+
+clear
+
+clear
+
+figlet hackinstall
+
+echo instalation sucessfuly!
+
+./startubuntu.sh
+
+cd ~
+
+rm hackinstall.sh
